67121bf2412d1a5f8fc083a7aba3c14e76168e86
guccialex <alex.gusain@gmail.com>
2019-04-01 18:12:31 -0400

    Add files via upload
    updated for a modern era

--- mainprogram.py
+++ mainprogram.py
@@ -1,72 +1,90 @@
 import os
 
-#gets the directory of the current path this python file is in
-cdirectory = os.path.dirname(os.path.realpath(__file__))
 
-#turns that string of the current location to bytes of it
-bytecdirectory = os.fsencode(cdirectory)
+#create a directory where the files will be saved
+os.system("mkdir OUTPUTFOLDER")
 
+currentfolder = os.getcwd()
 
-#if it doesnt exist, create the directory in the current directory that will store the created text and html files
-if (not os.path.exists(cdirectory)):
-	os.makedirs(cdirectory+"/htmlandtext")
+outputfolder = currentfolder + "/OUTPUTFOLDER"
 
-#the name of the path the new folder that will hold the created html and text files
-newfolder = cdirectory + "/htmlandtext/"
-
-#open a new text and html file in the newfolder directory
-createdtext = open(newfolder+"APPENEDTEXT.txt", "w")
-createdhtml = open(newfolder+"APPENEDHTML.html", "w")
-
-
-#remembers if this is the first loop and if we've put the header at the beginning of the html
-isheaderentered = False
-
-
-#goes over each html file in the current directory
-for file in os.listdir(cdirectory):
-    filename = os.fsdecode(file)
-    #print(filename)
-
-
-    #if the file in this directory ends with .html it's one of the keep files
-    if (filename.endswith(".html")):
-
-    	#open the current html file stored in filename that's being looped over to read its contents
-	    fileopened = open(filename, "r",encoding = "ISO-8859-1")
-	    #get the text in that file
-	    text = fileopened.read()
-
-	    #gets the two parts of the html text, one for everything before and one for everything after the first <body> tag
-	    beforeandafterbody = text.split("<body>",1)
-	    #stores everything after the first body tag in afterbody
-	    afterbody = beforeandafterbody[1]
-
-
-	    #if the header hasn't been put in yet, we put the header in now
-	    if (isheaderentered == False):
-    		createdhtml.write(beforeandafterbody[0])
-    		createdhtml.write("<body>")
-    		isheaderentered = True
+inputfolder = currentfolder + "/Keep"
 
 
 
-	    #Creates two indexes of everything before the last body tag and everything after the last body tag that was stored in afterbody
-	    midbodyandfooter = afterbody.split("</body>")
-	    #Stores everything between the first <body> tag and the last </body> tag in midbody
-	    midbody = midbodyandfooter[0]
+#copy the header file into the newfolder as the output file
+os.system("cp NOTEHEADER.html ./OUTPUTFOLDER")
 
 
-	    #writes the midbody to the html file
-	    createdhtml.write(midbody)
-	    createdtext.write(midbody)
-	    
-	    #closes the current keep html file being read
-	    fileopened.close()
+#open a new text and html file in the outputfolder directory
 
-#adds in the closing body tags to the html file
-createdhtml.write("</body>")
-createdhtml.write("</html>")
+outputtext = open(outputfolder+"/APPENEDTEXT.txt", "w+")
 
-createdhtml.close()
-createdtext.close()
+outputhtml = open(outputfolder+"/NOTEHEADER.html", "a+")
+
+
+
+
+#get each file inside the input folder
+
+filesininput = os.listdir("Keep")
+
+
+numberoffiles = len(filesininput)
+
+
+
+fileslookedat = 0
+
+
+
+#for each of the files in the input directory
+
+
+while (fileslookedat < numberoffiles):
+
+	currentfile = filesininput [fileslookedat]
+
+	fileslookedat += 1
+
+	#if the file is an html
+	if (currentfile[-4:] == "html"):
+
+		properfilename = inputfolder + "/" + currentfile
+
+		#open the file 
+		filecurrentfile = open(properfilename, "r")
+
+		#put the contents into this variable
+		currentfilecontents = filecurrentfile.read()
+
+		#close the current file
+		filecurrentfile.close()
+
+
+		#get the stuff after the first <body>
+		notebetweenbodybrackets = currentfilecontents.split("<body>", 1) [1]
+
+		#get the stuff before the first </body>
+		notebetweenbodybrackets = notebetweenbodybrackets.split("</body>", 1) [0]
+
+		#print(notebetweenbodybrackets)
+
+
+		#append to the output html
+		outputhtml.write(notebetweenbodybrackets)
+
+
+		#remove all the tags from the stuff in the body tag
+		#except for the <br> tags which i need to convert to line breaks
+		#basically convert all the tags I can and remove the others
+		#then append to the outputtext.txt
+
+
+
+#after all the appending, end it with a body tag and close everything
+outputhtml.write("</body>")
+
+outputhtml.close()
+
+outputtext.close()
